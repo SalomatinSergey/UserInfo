0aa8246ede402a8dc6ca45570df61b2109b4847c
Sergey Salomatin <salomatinsv8@gmail.com>
2022-05-16 10:50:30 +0300

    Create simple MVC app
    This app is created for learning mvvm architecture

---
+++ UserInfo/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  UserInfo
+//
+//  Created by Sergey on 16.05.2022.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
